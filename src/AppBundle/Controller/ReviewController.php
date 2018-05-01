64fa2500c50dcf4ec7c3fd3bcf890de3777da874
StevenGrl <steven.grillon.45@gmail.com>
2018-05-01 19:03:45 +0200

    quête routing

---
+++ src/AppBundle/Controller/ReviewController.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder4
+ * Date: 01/05/18
+ * Time: 18:43
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Review;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Reservation controller.
+ *
+ * @Route("review")
+ */
+
+class ReviewController extends Controller
+{
+    /**
+     * Lists all review entities.
+     *
+     * @Route("/", name="review_index")
+     * @Method("GET")
+    */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $reviews = $em->getRepository('AppBundle:Review')->findAll();
+
+        return $this->render('review/index.html.twig', array(
+            'reviews' => $reviews,
+        ));
+    }
+
+    /**
+     * Creates a new review entity.
+     *
+     * @Route("/new", name="review_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $review = new Review();
+        $form = $this->createForm('AppBundle\Form\ReviewType', $review);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($review);
+            $em->flush();
+
+            return $this->redirectToRoute('review_show', array('id' => $review->getId()));
+        }
+
+        return $this->render('review/new.html.twig', array(
+            'review' => $review,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a review entity.
+     *
+     * @Route("/{id}", name="review_show")
+     * @Method("GET")
+     */
+    public function showAction(Review $review)
+    {
+        $deleteForm = $this->createDeleteForm($review);
+
+        return $this->render('review/show.html.twig', array(
+            'review' => $review,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+}
